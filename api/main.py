a3a95dddb33e9ff5259b07cc2c83137c6bcafcde
marek.tremel <tremelmarek@gmail.com>
2025-03-20 16:38:33 +0100

    start project

--- api/main.py
+++ api/main.py
@@ -6,22 +6,3 @@ from fastapi import FastAPI
 app = FastAPI(
     docs_url="/",
 )
-
-
-# Decorátor pro vytvoření endpointu
-# Decorátor app.get() vytvoří endpoint pro GET request
-# GET /asd
-# Funkce read_root() vrací slovník
-# {"Hello": "World"}
-# Tento slovník se automaticky převede na JSON
-# a odešle klientovi
-@app.get("/asd")
-def read_root() -> dict[str, str]:
-    return {"Hello": "World"}
-
-
-@app.get("/asd/{asd_id}")
-def read_item(asd_id) -> dict:
-    return {
-        "asd_id": asd_id,
-    }
