9a767e84b92e0c26698618087b977417602ff9fd
marek.tremel <tremelmarek@gmail.com>
2025-03-24 21:27:01 +0100

    ano

--- api/utils.py
+++ api/utils.py
@@ -2,9 +2,11 @@ from fastapi import HTTPException
 
 
 def validate_int(number: int) -> int:
+    
     try:
-        number_str = str(number)
-        number_int = int(number_str, base=8)
-        return number_int
+        if number > 9223372036854775807 or number < -9223372036854775807:
+            raise ValueError("Number must be greater than 0")
+
+        return number
     except ValueError as e:
-        raise HTTPException(status_code=400, detail="Invalid number") from e
+        raise HTTPException(status_code=400, detail=e) from e
