0c47373ca93424a48f980311a96c286e4f5f6380
marek.tremel <tremelmarek@gmail.com>
2025-04-07 19:17:42 +0200

    middleware
    Adds logging and request processing middleware
    
    Introduces logging functionality with various log levels
    Includes middleware for logging requests and measuring processing time
    Updates Docker configuration to include log directory
    
    Relates to middleware improvements

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
+import time
 from fastapi import FastAPI
 from api.database import SqlSessionDependency, init_db
 from sqlalchemy import text
 
 from api.src.routers import router
+from api.logger import logger
 
 # Vytvoření instance FastAPI
 # docs_url="/" - nastavení URL pro dokumentaci
@@ -31,3 +33,32 @@ def health_check(
 # router - router, který obsahuje definice endpointů
 # include_router - metoda pro připojení routeru k aplikaci
 app.include_router(router=router)
+
+
+# Middlewares
+# Slouží pro zpracování požadavků a odpovědí
+# Middleware - funkce, která se vykonává před a po zpracování požadavku
+# Lze použít pro logování, autentizaci, atd.
+@app.middleware("http")
+async def add_rpcess_time_header(request, call_next):
+    """
+    Middleware pro přidání hlavičky s časem zpracování
+    """
+    start_time: float = time.time()
+    response = await call_next(request)
+    process_time: float = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+@app.middleware("http")
+async def logging_middleware(request, call_next):
+    """
+    Middleware pro logování požadavků
+    """
+    response = await call_next(request)
+    logger.info(
+        f"Request: {request.method} {request.url} - "
+        f"Response status: {response.status_code}"
+    )
+    return response
--- tasks.md
+++
@@ -1,3 +0,0 @@
-# Úkol na této větvi, která se soustředí na testování
-
-## Otestovat vytvořený crud a pokusit se vyčíst z reportu, co je špatně a to opravit :)
